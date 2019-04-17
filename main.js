8d844e3e4666f98f6d7d4fa8789fb33138627d19
David Mora <David.mora@codeimmersives.com>
2019-04-17 15:45:34 -0400

    finished functions to populate data based on which button is clicked. added more data to make the page seem more full

--- main.js
+++ main.js
@@ -12,14 +12,36 @@ function init() {
 
 function toggleCity(city) {
     document.querySelector('#city-name').innerText = city["name"];
+    document.querySelector('.population').innerText = city["population"];
+    document.querySelector('.map').src = city["tableImage"];
+    document.querySelector('.founded').innerText = city["founded"];
+    document.querySelector('#history').innerText = city["history"];
+    document.querySelector('#climate').innerText = city["climate"];
 
 }
 
 function populateHTML() {
-    document.querySelector('#history').innerText = newYork["history"];
+    event.preventDefault();
 
-    const map = document.createElement('td');
-    const appendElement = document.querySelector('#table-map');
-    map.innerText = 'hello';
-    appendElement.appendChild('map')
+    document.querySelector('#history').innerText = newYork["history"];
+    document.querySelector('#city-name').innerText = newYork["name"];
+    document.querySelector("#climate").innerText = newYork["climate"];
+
+    const map = document.createElement('IMG');
+    appendElement = document.querySelector('#table-map');
+    map.src = newYork["tableImage"];
+    map.className = "map";
+    appendElement.appendChild(map);
+
+    const population = document.createElement('td');
+    appendElement = document.querySelector('#table-pop');
+    population.innerText = newYork["population"];
+    population.className = "population";
+    appendElement.appendChild(population);
+
+    const founded = document.createElement('td');
+    appendElement = document.querySelector('#table-founded');
+    founded.innerText = newYork["founded"];
+    founded.className = "founded";
+    appendElement.appendChild(founded);
 }
