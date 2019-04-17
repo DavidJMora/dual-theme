f95af622a6d997f97e2378997a8dac24eba1363e
David Mora <David.mora@codeimmersives.com>
2019-04-17 14:07:26 -0400

    created new function to call upon onload that populates default HTML

---
+++ data.js
@@ -0,0 +1,15 @@
+const newYork = {
+    "name": "New York City, NY",
+    "tableImage": "./images/new york map.gif",
+    "population": "8.6 million people",
+    "founded": "~1609",
+    "history": "The 'Sons of Liberty' destroyed British authority in New York City, and the Stamp Act Congress of representatives from throughout the Thirteen Colonies met in the city in 1765 to organize resistance to British policies. The city's strategic location and status as a major seaport made it the prime target for British seizure in 1776. General George Washington lost a series of battles from which he narrowly escaped (with the notable exception of the Battle of Harlem Heights, his first victory of the war), and the British Army controlled New York City and made it their base on the continent until late 1783, attracting Loyalist refugees. The city served as the national capital under the Articles of Confederation from 1785-1789, and briefly served as the new nation's capital in 1789–90 under the United States Constitution. Under the new government the city hosted the inauguration of George Washington as the first President of the United States, the drafting of the United States Bill of Rights, and the first Supreme Court of the United States. The opening of the Erie Canal gave excellent steamboat connections with upstate New York and the Great Lakes, along with coastal traffic to lower New England, making the city the preeminent port on the Atlantic Ocean. The arrival of rail connections to the north and west in the 1840s and 1850s strengthened its central role."
+}
+
+const lawrence = {
+    "name": "Lawrence, KS",
+    "tableImage": "/images/lawrence map.gif",
+    "population": "~100,000",
+    "founded": "1854",
+    "history": "Lawrence was founded 'strictly for political reasons' having to do with the issue of slavery, which was heavily debated in the United States during the early-to-mid 1800s. Northern Democrats, led by Senators Lewis Cass of Michigan and Stephen A. Douglas of Illinois promoted the idea of 'popular sovereignty' as a middle position on the slavery issue. Proponents of this doctrine argued that it was more democratic, as it allowed the citizens of newly-organized territories (and not politicians from Washington, D.C.) to have final say in regards to the permissibly of slavery in their own lands. (Meanwhile, enemies of the bill, especially those in the north, derisively called this political idea 'squatter sovereignty'.)Douglas eventually made popular sovereignty the backbone of his Kansas–Nebraska Act—legislation that effectively repealed the Missouri Compromise of 1820 and created the territories of Kansas and Nebraska—which finally passed in Congress in 1854"
+}
--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-windows.onload = init;
+window.onload = init;
 
 function init() {
+    populateHTML();
+
     document.querySelector('.new-york')
-        .addEventListener('click', changeNewYork);
+        .addEventListener('click', () => toggleCity(newYork));
 
     document.querySelector('.lawrence')
-        .addEventListener('click', changeLawrence);
+        .addEventListener('click', () => toggleCity(lawrence));
+}
+
+function toggleCity(city) {
+    document.querySelector('#city-name').innerText = city["name"];
+
+}
+
+function populateHTML() {
+    document.querySelector('#history').innerText = newYork["history"];
+
+    const map = document.createElement('td');
+    const appendElement = document.querySelector('#table-map');
+    map.innerText = 'hello';
+    appendElement.appendChild('map')
 }
