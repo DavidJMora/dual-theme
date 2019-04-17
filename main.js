77a17af5f267154f19068f0002da686277fd8fb5
David Mora <David.mora@codeimmersives.com>
2019-04-17 16:42:41 -0400

    created two new functions that disable the buttons once they've been clicked to force user to only click on the opposite button

--- main.js
+++ main.js
@@ -4,10 +4,28 @@ function init() {
     populateHTML();
 
     document.querySelector('.new-york')
-        .addEventListener('click', () => toggleCity(newYork));
+        .addEventListener('click', toggleNY);
 
     document.querySelector('.lawrence')
-        .addEventListener('click', () => toggleCity(lawrence));
+        .addEventListener('click', toggleLawrence);
+}
+
+function toggleNY(event) {
+    const lawrenceButton = document.querySelector('.lawrence');
+    const nyButton = document.querySelector('.new-york');
+    lawrenceButton.disabled = false;
+    nyButton.disabled = true;
+
+    toggleCity(newYork);
+}
+
+function toggleLawrence(event) {
+    const lawrenceButton = document.querySelector('.lawrence');
+    const nyButton = document.querySelector('.new-york');
+    lawrenceButton.disabled = true;
+    nyButton.disabled = false;
+
+    toggleCity(lawrence);
 }
 
 function toggleCity(city) {
@@ -17,11 +35,16 @@ function toggleCity(city) {
     document.querySelector('.founded').innerText = city["founded"];
     document.querySelector('#history').innerText = city["history"];
     document.querySelector('#climate').innerText = city["climate"];
+    document.querySelector('.mayor').innerText = city["mayor"];
+    document.querySelector('.elevation').innerText = city["elevation"];
 
 }
 
 function populateHTML() {
     event.preventDefault();
+
+    const button = document.querySelector('.new-york');
+    button.disabled = true;
 
     document.querySelector('#history').innerText = newYork["history"];
     document.querySelector('#city-name').innerText = newYork["name"];
@@ -36,12 +59,24 @@ function populateHTML() {
     const population = document.createElement('td');
     appendElement = document.querySelector('#table-pop');
     population.innerText = newYork["population"];
-    population.className = "population";
+    population.className = "population table-info";
     appendElement.appendChild(population);
 
     const founded = document.createElement('td');
     appendElement = document.querySelector('#table-founded');
     founded.innerText = newYork["founded"];
-    founded.className = "founded";
+    founded.className = "founded table-info";
     appendElement.appendChild(founded);
+
+    const mayor = document.createElement('td');
+    appendElement = document.querySelector('#table-mayor');
+    mayor.innerText = newYork["mayor"];
+    mayor.className = "mayor table-info";
+    appendElement.appendChild(mayor);
+
+    const elevation = document.createElement('td');
+    appendElement = document.querySelector('#table-elevation');
+    elevation.innerText = newYork["elevation"];
+    elevation.className = "elevation table-info";
+    appendElement.appendChild(elevation);
 }
